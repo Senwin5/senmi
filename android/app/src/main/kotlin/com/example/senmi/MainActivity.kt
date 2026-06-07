fec5cfa0d1636b041d0add7c3084fa670af4c5d5
Senwin.ng <godwinsenwin@outlook.com>
2026-06-07 16:11:24 +0100

    A new role has been added to the database

--- android/app/src/main/kotlin/com/example/senmi/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.senmi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/senmi/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.senmi.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
